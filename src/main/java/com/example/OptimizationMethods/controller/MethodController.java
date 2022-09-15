3ebe31b0d43c2991693b929f6619b036266578b8
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-09-16 01:04:55 +0300

    modified function entity: constructor and calculateValueByParametr method

--- src/main/java/com/example/OptimizationMethods/controller/MethodController.java
+++ src/main/java/com/example/OptimizationMethods/controller/MethodController.java
@@ -1,6 +1,11 @@
 package com.example.OptimizationMethods.controller;
 
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 import lombok.NonNull;
+import org.apache.el.lang.FunctionMapperImpl;
+import org.apache.el.lang.FunctionMapperImpl.Function;
 import org.springframework.boot.context.properties.bind.Name;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,13 +30,18 @@ public class MethodController {
 
         double result = 0;
         boolean stopCreterion = false;
+        Function function = new Function();
         while(!stopCreterion){
             double x1 = ((startingPoint + finishingPoint) / 2)  - constantDistinguishability;
             double x2 = ((startingPoint + finishingPoint) / 2)  + constantDistinguishability;
+            double y1 = function.calculateValueByParametr(x1);
+            double y2 = function.calculateValueByParametr(x2);
+            /*
             double y1 = x1*x1 - 2*x1 +1;
             double y2 = x2*x2 - 2*x2 +1;
-            if(y1 <= y2){
+             */
 
+            if(y1 <= y2){
                 finishingPoint = x2;
             } else {
                 startingPoint = x1;
@@ -45,25 +55,102 @@ public class MethodController {
     }
 
     /*
-    Формат записи функции следующий y = x^2 - 2 * x + 1
+    Формат записи функции следующий y = (x^2) - (2 * x) + (1)
      */
-    class Function{
+    public class Function {
+        public void Function (/*String stringFunction*/) {
+            GeneralInfo generalInfo = new GeneralInfo();
+            generalInfo.moduleCount = 3;
+            generalInfo.intermoduleOperators = new ArrayList(Arrays.asList("-","+"));
+            modules = new ArrayList<>();
+            /*
+            Создаем первый модуль
+             */
+                FunctionModule functionModule_1 = new FunctionModule();
+                functionModule_1.info.existenceVariable = true;
+                functionModule_1.info.notNull = true;
+                functionModule_1.info.trigonometricFunction = false;
+                functionModule_1.info.exponentiation = true;
+                functionModule_1.info.factorAvailability = true;
+                functionModule_1.options.trigonometric = null;
+                functionModule_1.options.exponentiation = 2;
+                functionModule_1.options.factor = 2;
+                /*
+                Создаем второй модуль
+                 */
+                FunctionModule functionModule_2 = new FunctionModule();
+                functionModule_2.info.existenceVariable = true;
+                functionModule_2.info.notNull = true;
+                functionModule_2.info.trigonometricFunction = false;
+                functionModule_2.info.exponentiation = false;
+                functionModule_2.info.factorAvailability = true;
+                functionModule_2.options.trigonometric = null;
+                functionModule_2.options.exponentiation = 2;
+                functionModule_2.options.factor = 2;
+                /*
+                Создаем третий модуль
+                 */
+                FunctionModule functionModule_3 = new FunctionModule();
+                functionModule_3.info.existenceVariable = false;
+                functionModule_3.info.notNull = true;
+                functionModule_3.info.trigonometricFunction = false;
+                functionModule_3.info.exponentiation = false;
+                functionModule_3.info.factorAvailability = true;
+                functionModule_3.options.trigonometric = null;
+                functionModule_3.options.exponentiation = 1;
 
-        /*
-        Ну типа конструктор
-         */
-        public Function (String stringFunction) {
-
+                modules.add(functionModule_1);
+                modules.add(functionModule_2);
+                modules.add(functionModule_3);
         }
-        private final class GeneralInfo{
-            private int moduleCount;
-            private ArrayList<String> intermoduleOperators[];
+        public class GeneralInfo {
+            public int moduleCount;
+            public List<String> intermoduleOperators;
+        }
+        public ArrayList<FunctionModule> modules;
+        public class FunctionModule {
+            public Info info;
+            public Options options;
+            public class Info {
+                public boolean existenceVariable;
+                public boolean notNull;
+                public boolean trigonometricFunction;
+                public boolean exponentiation;
+                public boolean factorAvailability;
+            }
+            public class Options {
+                public String trigonometric;
+                public double exponentiation;
+                public double factor;
+            }
         }
 
-        private ArrayList<FunctionModule> modules;
-
-        private class FunctionModule {
-
+        public double calculateValueByParametr (double parametr){
+            double result = 0;
+            for(int i = 0; i < modules.size(); i++){
+                double moduleResult = 0;
+                if(modules.get(i).info.existenceVariable){
+                    moduleResult = parametr;
+                } else {
+                    moduleResult = 1;
+                }
+                if(modules.get(i).info.exponentiation){
+                    moduleResult = Math.pow(parametr, modules.get(i).options.exponentiation);
+                }
+                if(modules.get(i).info.factorAvailability){
+                    moduleResult = moduleResult * modules.get(i).options.factor;
+                }
+                if(i == 0){
+                    result = moduleResult;
+                }
+                if(i == 1){
+                    result -= moduleResult;
+                }
+                if(i== 2){
+                    result += moduleResult;
+                }
+            }
+            return result;
         }
     }
 }
