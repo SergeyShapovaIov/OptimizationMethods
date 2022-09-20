f42c87de58f8eb1052dc4fee02ef3465a9059e13
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-09-21 01:00:26 +0300

    modified function constructor

--- src/main/java/com/example/OptimizationMethods/controller/MethodController.java
+++ src/main/java/com/example/OptimizationMethods/controller/MethodController.java
@@ -2,19 +2,14 @@ package com.example.OptimizationMethods.controller;
 
 import com.example.OptimizationMethods.controller.MethodController.Function.FunctionModule.Info;
 import com.example.OptimizationMethods.controller.MethodController.Function.FunctionModule.Options;
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 import lombok.NonNull;
-import org.apache.el.lang.FunctionMapperImpl;
-import org.apache.el.lang.FunctionMapperImpl.Function;
-import org.springframework.boot.context.properties.bind.Name;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
+
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/method")
@@ -32,7 +27,7 @@ public class MethodController {
 
         double result = 0;
         boolean stopCreterion = false;
-        Function function = new Function();
+        Function function = new Function("y= (2*(x^2)) - (2*x) + (1)");
         while(!stopCreterion){
             double x1 = ((startingPoint + finishingPoint) / 2)  - constantDistinguishability;
             double x2 = ((startingPoint + finishingPoint) / 2)  + constantDistinguishability;
@@ -59,15 +54,55 @@ public class MethodController {
     Формат записи функции следующий y = (x^2) - (2 * x) + (1)
      */
     public class Function {
-        public Function () {
-
+        public Function (String functionString) {
+            // Создаем объект с общей информацией для всех модулей
             GeneralInfo generalInfo = new GeneralInfo();
-            generalInfo.moduleCount = 3;
-            generalInfo.intermoduleOperators = new ArrayList(Arrays.asList("-","+"));
+            // Создаем список с модулями функции
+            ArrayList<String> functionModules = new ArrayList<String>(Arrays.asList(functionString.split(" ")));
+            ArrayList<String> intermoduleOperatorsList = new ArrayList<>();
+            ArrayList<String> calculationModule = new ArrayList<>();
+            // Заполняем значение поля Info
+            if(!functionModules.isEmpty()){
+                if(functionModules.size() > 2){
+                    for(int i = 2 ; i < functionModules.size(); i+=2){
+                        intermoduleOperatorsList.add(functionModules.get(i));
+                    }
+                }
+                for(int i = 1; i < functionModules.size(); i+=2){
+                    calculationModule.add(functionModules.get(i));
+                }
+            }
+            generalInfo.moduleCount = intermoduleOperatorsList.size();
+            generalInfo.intermoduleOperators = intermoduleOperatorsList;
             this.modules = new ArrayList<>();
             /*
             Создаем первый модуль
              */
+            for(int moduleNumber = 0; moduleNumber < generalInfo.getModuleCount(); moduleNumber ++){
+                FunctionModule functionModule = new FunctionModule();
+                functionModule.info = new Info();
+                functionModule.options = new Options();
+
+                Info info = functionModule.info;
+                Options options = functionModule.options;
+
+                if(calculationModule.get(moduleNumber).contains("x")) {
+                    info.existenceVariable = true;
+                } else {
+                    info.existenceVariable = false;
+                }
+
+                if(calculationModule.get(moduleNumber).isEmpty()) {
+                    info.notNull = false;
+                } else {
+                    info.notNull = true;
+                }
+
+
+
+
+                this.modules.add(functionModule);
+            }
                 FunctionModule functionModule_1 = new FunctionModule();
                 functionModule_1.info  = new Info();
                 functionModule_1.options = new Options();
@@ -113,13 +148,43 @@ public class MethodController {
 
         }
         public class GeneralInfo {
+
+            public int getModuleCount() {
+                return this.moduleCount;
+            }
+
+            public void setModuleCount(final int moduleCount) {
+                this.moduleCount = moduleCount;
+            }
+
+            public List<String> getIntermoduleOperators() {
+                return this.intermoduleOperators;
+            }
+
+            public void setIntermoduleOperators(final List<String> intermoduleOperators) {
+                this.intermoduleOperators = intermoduleOperators;
+            }
+
             public int moduleCount;
             public List<String> intermoduleOperators;
 
             public GeneralInfo(){
 
             }
+
+
+
         }
+
+        public ArrayList<FunctionModule> getModules() {
+            return this.modules;
+        }
+
+        public void setModules(
+            final ArrayList<FunctionModule> modules) {
+            this.modules = modules;
+        }
+
         public ArrayList<FunctionModule> modules;
         public class FunctionModule {
             public Info info;
