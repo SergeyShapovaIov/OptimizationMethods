c16d489de497120c28c2e82dee336348bd565317
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-09-27 15:23:52 +0300

    stable condition DSC method

--- src/main/java/com/example/OptimizationMethods/controller/MethodController.java
+++ src/main/java/com/example/OptimizationMethods/controller/MethodController.java
@@ -1,55 +1,57 @@
 package com.example.OptimizationMethods.controller;
 
-import com.example.OptimizationMethods.controller.MethodController.Function.FunctionModule;
-import com.example.OptimizationMethods.controller.MethodController.Function.FunctionModule.Info;
-import com.example.OptimizationMethods.controller.MethodController.Function.FunctionModule.Options;
-import java.net.URLDecoder;
-import java.util.Arrays;
-import java.util.List;
-import lombok.NonNull;
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.*;
-
-
+import com.example.OptimizationMethods.Function;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.NonNull;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/method")
 public class MethodController {
 
-    private static final Logger log = Logger.getLogger(MethodController.class);
-
-    /*
-     Вот для этой функции считаем:  y = x^2 - 2 * x + 1
-     */
-
     @PostMapping("/halving")
-    public double halving (@RequestParam @NonNull String functionString
-            , @RequestParam @NonNull double searchAccuracy
-            , @RequestParam @NonNull double constantDistinguishability
-            , @RequestParam @NonNull double startingPoint
-            , @RequestParam @NonNull double finishingPoint) {
+    public double halving(@RequestParam @NonNull String functionString
+        , @RequestParam @NonNull double searchAccuracy
+        , @RequestParam @NonNull double constantDistinguishability
+        , @RequestParam @NonNull double startPosition
+        , @RequestParam @NonNull double step) {
+
+
         functionString = functionDecoder(functionString);
+
+        Map<String,String> paramsDSC = new HashMap<String, String>();
+        paramsDSC.put("functionString", functionString);
+        paramsDSC.put("startPosition", String.valueOf(startPosition));
+        paramsDSC.put("step",String.valueOf(step));
+
+        double segmentList[] = DSC(paramsDSC);
+        double startingPoint = segmentList[0];
+        double finishingPoint = segmentList[1];
+
         double result = 0;
         boolean stopCreterion = false;
         Function function = new Function(functionString);
-        while(!stopCreterion){
-            double x1 = ((startingPoint + finishingPoint) / 2)  - constantDistinguishability;
-            double x2 = ((startingPoint + finishingPoint) / 2)  + constantDistinguishability;
+        while (!stopCreterion) {
+            double x1 = ((startingPoint + finishingPoint) / 2) - constantDistinguishability;
+            double x2 = ((startingPoint + finishingPoint) / 2) + constantDistinguishability;
             double y1 = function.calculateValueByParametr(x1);
-            System.out.println("Input param :"  +x1 + "  Output param: " +y1);
-            log.info("Input param : " +x1 + ", Output param: " +y1);
+            System.out.println("Input param :" + x1 + "  Output param: " + y1);
             double y2 = function.calculateValueByParametr(x2);
-            System.out.println("Input param :"  +x2 + "   Output param: " +y2);
-            log.info("Input param : " +x2 + ", Output param: " +y2);
-            if(y1 <= y2){
+            System.out.println("Input param :" + x2 + "   Output param: " + y2);
+            if (y1 <= y2) {
                 finishingPoint = x2;
             } else {
                 startingPoint = x1;
             }
-            if(((finishingPoint - startingPoint)/2 ) < searchAccuracy){
-                result = (startingPoint + finishingPoint) /2 ;
+            if (((finishingPoint - startingPoint) / 2) < searchAccuracy) {
+                result = (startingPoint + finishingPoint) / 2;
                 stopCreterion = true;
             }
         }
@@ -57,250 +59,76 @@ public class MethodController {
         return result;
     }
 
-    /*
-    Формат записи функции следующий y = (x^2) - (2 * x) + (1)
-     */
-    public class Function {
-        public Function (String functionString) {
-            // Создаем объект с общей информацией для всех модулей
-            GeneralInfo generalInfo = new GeneralInfo();
-            // Создаем список с модулями функции
-            ArrayList<String> functionModules = new ArrayList<String>(Arrays.asList(functionString.split(" ")));
-            ArrayList<String> intermoduleOperatorsList = new ArrayList<>();
-            ArrayList<String> calculationModule = new ArrayList<>();
-            // Заполняем значение поля Info
-            if(!functionModules.isEmpty()){
-                if(functionModules.size() > 2){
-                    for(int i = 2 ; i < functionModules.size(); i+=2){
-                        intermoduleOperatorsList.add(functionModules.get(i));
-                    }
-                }
-                for(int i = 1; i < functionModules.size(); i+=2){
-                    calculationModule.add(functionModules.get(i));
-                }
-            }
-            generalInfo.moduleCount = intermoduleOperatorsList.size();
-            generalInfo.intermoduleOperators = intermoduleOperatorsList;
-            this.modules = new ArrayList<>();
-            /*
-            Создаем первый модуль
-             */
-            for(int moduleNumber = 0; moduleNumber <= generalInfo.getModuleCount(); moduleNumber ++){
-                FunctionModule functionModule = new FunctionModule();
-                functionModule.info = new Info();
-                functionModule.options = new Options();
-
-                Info info = functionModule.info;
-                Options options = functionModule.options;
-
-                // existenceVariable
-                if(calculationModule.get(moduleNumber).contains("x")) {
-                    info.existenceVariable = true;
-                } else {
-                    info.existenceVariable = false;
-                }
-
-                // notNull
-                if(calculationModule.get(moduleNumber).isEmpty()) {
-                    info.notNull = false;
-                } else {
-                    info.notNull = true;
-                }
-
-                // trigonometricFunction
-                String[] trigonometricFunctionArray= {"sin","cos","tg","ctg"};
-                for(int i = 0; i<trigonometricFunctionArray.length; i++){
-                    if(calculationModule.get(moduleNumber).contains(trigonometricFunctionArray[i])){
-                        info.trigonometricFunction = true;
-                        break;
-                    } else {
-                        info.trigonometricFunction = false;
-                    }
-                }
-
-                // exponent
-                if(calculationModule.get(moduleNumber).contains("^")){
-                    info.exponent = true;
-                } else {
-                    info.exponent = false;
-                }
-
-                // factorAvailability
-                if(calculationModule.get(moduleNumber).contains("*")){
-                    info.factorAvailability = true;
-                } else {
-                    info.factorAvailability  =false;
-                }
-
-                // values trigonometric
-                if(info.trigonometricFunction){
-                    for(int i = 0; i < trigonometricFunctionArray.length; i++){
-                        if(calculationModule.get(moduleNumber).contains(trigonometricFunctionArray[i])){
-                            options.trigonometric = trigonometricFunctionArray[i];
-                        }
-                    }
-                } else {
-                    options.trigonometric = null;
-                }
-
-                if(info.exponent){
-                    options.exponentiation = Character.digit(
-                            calculationModule.get(moduleNumber)
-                            .charAt(calculationModule.get(moduleNumber).indexOf("^")+1)
-                            ,10);
-                } else {
-                    options.exponentiation = 1;
-                }
-
-                if(info.factorAvailability){
-                    options.factor =  Character.digit(
-                            calculationModule.get(moduleNumber)
-                            .charAt(calculationModule.get(moduleNumber).indexOf("*")-1)
-                    ,10) ;
-                } else {
-                    options.factor = 1;
-                }
-
-
-                this.modules.add(functionModule);
-            }
-        }
-        public class GeneralInfo {
-
-            public int getModuleCount() {
-                return this.moduleCount;
-            }
-
-            public void setModuleCount(final int moduleCount) {
-                this.moduleCount = moduleCount;
-            }
-
-            public List<String> getIntermoduleOperators() {
-                return this.intermoduleOperators;
-            }
-
-            public void setIntermoduleOperators(final List<String> intermoduleOperators) {
-                this.intermoduleOperators = intermoduleOperators;
-            }
-
-            public int moduleCount;
-            public List<String> intermoduleOperators;
-
-            public GeneralInfo(){
-
-            }
-
-
-
-        }
-
-        public ArrayList<FunctionModule> getModules() {
-            return this.modules;
-        }
-
-        public void setModules(
-            final ArrayList<FunctionModule> modules) {
-            this.modules = modules;
-        }
-
-        public ArrayList<FunctionModule> modules;
-        public class FunctionModule {
-            public Info info;
-            public Options options;
-            public FunctionModule(){
-
-            }
-            public static class Info {
-
-                public boolean isExistenceVariable() {
-                    return this.existenceVariable;
-                }
-
-                public void setExistenceVariable(final boolean existenceVariable) {
-                    this.existenceVariable = existenceVariable;
-                }
-
-                public boolean isNotNull() {
-                    return this.notNull;
-                }
-
-                public void setNotNull(final boolean notNull) {
-                    this.notNull = notNull;
-                }
-
-                public boolean isTrigonometricFunction() {
-                    return this.trigonometricFunction;
-                }
-
-                public void setTrigonometricFunction(final boolean trigonometricFunction) {
-                    this.trigonometricFunction = trigonometricFunction;
-                }
-
-                public boolean isExponent() {
-                    return this.exponent;
-                }
-
-                public void setExponent(final boolean exponent) {
-                    this.exponent = exponent;
-                }
-
-                public boolean isFactorAvailability() {
-                    return this.factorAvailability;
-                }
-
-                public void setFactorAvailability(final boolean factorAvailability) {
-                    this.factorAvailability = factorAvailability;
-                }
-
-                public boolean existenceVariable;
-                public boolean notNull;
-                public boolean trigonometricFunction;
-                public boolean exponent;
-                public boolean factorAvailability;
-
-                public Info(){
-
-                }
-            }
-            public static class Options {
-                public String trigonometric;
-                public double exponentiation;
-                public double factor;
-
-                public Options(){
-
-                }
-            }
-        }
-
-        public double calculateValueByParametr (double parametr){
-            double result = 0;
-            for(int i = 0; i < modules.size(); i++){
-                double moduleResult = 0;
-                if(modules.get(i).info.existenceVariable){
-                    moduleResult = parametr;
-                } else {
-                    moduleResult = 1;
-                }
-                if(modules.get(i).info.exponent){
-                    moduleResult = Math.pow(parametr, modules.get(i).options.exponentiation);
-                }
-                if(modules.get(i).info.factorAvailability){
-                    moduleResult = moduleResult * modules.get(i).options.factor;
-                }
-
-            }
-            return result;
-        }
-    }
-
-    public String functionDecoder (String str){
+    public String functionDecoder(String str) {
         String resultString;
-        if(str.contains("**")){
-            resultString = str.replace("**","^");
+        if (str.contains("**")) {
+            resultString = str.replace("**", "^");
         } else {
             resultString = str;
         }
         return resultString;
+    }
+
+
+    private double[] DSC(Map<String,String> paramsDSC) {
+
+        String functionString = paramsDSC.get("functionString");
+        double startPosition = Double.valueOf(paramsDSC.get("startPosition"));
+        double step = Double.valueOf(paramsDSC.get("step"));
+
+        functionString = functionDecoder(functionString);
+        Function function = new Function(functionString);
+        double segmentLimits[] = new double[2];
+        List<Double> xParam = new ArrayList<>();
+        xParam.add(startPosition);
+        // 1 step
+        double y1 = function.calculateValueByParametr(startPosition);
+        double y2 = function.calculateValueByParametr(startPosition + step);
+        int counter;
+        // 2 step
+        if (y1 > y2) {
+            segmentLimits[0] = startPosition;
+            xParam.add(1, startPosition + step);
+            counter = 2;
+        } else {
+            // 3 step
+            double y3 = function.calculateValueByParametr(startPosition - step);
+            if (y3 >= y1) {
+                segmentLimits[0] = startPosition - step;
+                segmentLimits[1] = startPosition + step;
+                return segmentLimits;
+            } else {
+                segmentLimits[1] = startPosition;
+                xParam.add(startPosition - step);
+                step = 0 - step;
+                counter = 2;
+            }
+        }
+        do {
+            xParam.add(xParam.get(0) + Math.pow(2, counter - 1) * step);
+            y1 = function.calculateValueByParametr(
+                xParam.get(counter)
+            );
+            y2 = function.calculateValueByParametr(
+                xParam.get(counter-1)
+            );
+            if(y2 <= y1){
+                if(step > 0){
+                    segmentLimits[1] = xParam.get(counter);
+                } else{
+                    segmentLimits[0] = xParam.get(counter);
+                }
+                break;
+            } else {
+                if (step > 0){
+                    segmentLimits[0] = xParam.get(counter-1);
+                } else {
+                    segmentLimits[1] = xParam.get(counter-1);
+                }
+                counter++;
+            }
+        } while (y2 > y1);
+
+        return segmentLimits;
     }
 }
